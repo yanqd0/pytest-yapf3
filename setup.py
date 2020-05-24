c8cb36ac85ae7184e79980a4fd2c21b02fbd693e
Yan QiDong <yanqd0@outlook.com>
2020-05-24 22:33:51 +0800

    Change setup.py to adapt Pipfile

--- setup.py
+++ setup.py
@@ -4,18 +4,6 @@
 
 from setuptools import find_packages, setup
 
-_TEST_REQUIRES = [
-    'mock',
-    'pytest',
-    'pytest-cov',
-    'pytest-docstyle<2',
-    'pytest-flake8',
-    'pytest-isort',
-    'pytest-mock',
-    'pytest-pycodestyle',
-    'pytest-pylint<0.16.0',
-]
-
 setup(
     use_scm_version=True,
     zip_safe=False,
@@ -27,38 +15,26 @@ setup(
         'pytest11': ('yapf = pytest_yapf3', ),
     },
     # Requires
-    python_requires='>=3.4',
-    install_requires=(
-        'pytest>=3.1.1',
-        'yapf>=0.16.2',
-    ),
+    python_requires='>=3.6',
     setup_requires=[
         'pytest-runner',
         'setuptools-scm',
     ],
-    tests_require=_TEST_REQUIRES,
-    extras_require={
-        'dev': _TEST_REQUIRES + [
-            'flake8',
-            'isort',
-            'pylint~=2.4.4',
-            'six~=1.12.0',
-        ],
+    use_pipfile={
+        'path': 'Pipfile',
+        'extras': True,
     },
     # PyPI Metadata
-    keywords=['pytest', 'yapf', 'sc'],
+    keywords=['pytest', 'yapf'],
     platforms=['any'],
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Framework :: Pytest',
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Testing',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: Implementation :: CPython',
         'Operating System :: OS Independent',
         'License :: OSI Approved :: MIT License',
     ],
